9c2646591a318353c445801f02752f0d59dc59e7
VolvoLife <134153519+VolvoLife@users.noreply.github.com>
2023-05-20 09:01:56 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,211 @@
+function ShowMenu() {
+
+document.getElementById("menu").classList.toggle("visible");
+
+document.getElementById("bodyDIM").classList.toggle("dim");
+}
+
+function quizform() {
+
+// get all the values //
+
+var cartype = document.getElementById("type").value;
+
+var carstyle = document.getElementById("style").value;
+
+var passenger = document.getElementById("pplnum").value;
+
+var price = document.getElementById("cash").value;
+
+
+
+// selection //
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "4" && price == "$2" ) {
+window.location.href = "../P1800/p1800.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "4" && price == "$2" ) {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "4" && price == "$2" ) {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "4" && price == "$1" ) {
+window.location.href = "../P1800/p1800.html"; }
+
+
+
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "5" && price == "$2") {
+window.location.href = "../Amazon/amazon.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "7" && price == "$2" ) {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "5" && price == "$1") {
+window.location.href = "../Amazon/amazon.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "7" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "4" && price == "$3") {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "5" && price == "$3") {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Classic" && passenger == "7" && price == "$3") {
+window.location.href = "null.html"; }
+
+
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "4" && price == "$2") {
+window.location.href = "../Sedans/s60.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "5" && price == "$2") {
+window.location.href = "../S80/s80.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "7" && price == "$2") {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "4" && price == "$1") {
+window.location.href = "../Sedans/s60.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "5" && price == "$1") {
+window.location.href = "../S80/s80.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "7" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "4" && price == "$3") {
+window.location.href = "../S90/s90.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "5" && price == "$3") {
+window.location.href = "../Sedans/s60.html"; }
+
+if(cartype == "Sedan" && carstyle == "Modern" && passenger == "7" && price == "$3") {
+window.location.href = "null.html"; }
+
+
+
+
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "4" && price == "$1") {
+window.location.href = "../100 Series/100.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "5" && price == "$1") {
+window.location.href = "../100 Series/100.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "7" && price == "$1") {
+window.location.href = "../200 Series/200.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "4" && price == "$2") {
+window.location.href = "../100 Series/100.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "5" && price == "$2") {
+window.location.href = "../100 Series/100.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "7" && price == "$2") {
+window.location.href = "../200 Series/200.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "4" && price == "$3") {
+window.location.href = "null.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "5" && price == "$3") {
+window.location.href = "null.html"; }
+
+if(cartype == "Wagon" && carstyle == "Classic" && passenger == "7" && price == "$3") {
+window.location.href = "null.html"; }
+
+
+
+
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "4" && price == "$1") {
+window.location.href = "../Wagons/xc70.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "5" && price == "$1") {
+window.location.href = "../V60/V60.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "7" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "4" && price == "$2") {
+window.location.href = "../Wagons/xc70.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "5" && price == "$2") {
+window.location.href = "../Wagons/xc70.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "7" && price == "$2") {
+window.location.href = "null.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "4" && price == "$3") {
+window.location.href = "../V60/V60.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "5" && price == "$3") {
+window.location.href = "../V60/V60.html"; }
+
+if(cartype == "Wagon" && carstyle == "Modern" && passenger == "7" && price == "$3") {
+window.location.href = "null.html"; }
+
+
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "4" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "5" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "7" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "4" && price == "$2") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "5" && price == "$2") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "7" && price == "$2") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "4" && price == "$3") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "5" && price == "$3") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Classic" && passenger == "7" && price == "$3") {
+window.location.href = "null.html"; }
+
+
+
+
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "4" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "5" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "7" && price == "$1") {
+window.location.href = "null.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "4" && price == "$2") {
+window.location.href = "../xc90/xc90.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "5" && price == "$2") {
+window.location.href = "../xc90/xc90.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "7" && price == "$2") {
+window.location.href = "../xc90/xc90.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "4" && price == "$3") {
+window.location.href = "../EX90/EX90.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "5" && price == "$3") {
+window.location.href = "../EX90/EX90.html"; }
+
+if(cartype == "SUV" && carstyle == "Modern" && passenger == "7" && price == "$3") {
+window.location.href = "../EX90/EX90.html"; }
+
+}
